01cadd6a84233669cb855fff78ff20c4360ef762
sqz023 <saqibzaheeer@gmail.com>
2018-01-23 19:57:51 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# kitchen-ec2
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: kitchen-ec2
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
